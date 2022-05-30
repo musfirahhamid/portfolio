c35b5e07c0834f41b4176af7caf2b5773bdebd2b
Musfirah Hamid <29musfirah@gmail.com>
2022-05-31 00:59:30 +0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{html,jsx}"],
+  theme: {
+    extend: {
+      colors:{
+        main:'#0A192F',
+        heading1:'#CCD6F6',
+        sky:'#64FFDA',
+        bgHover:'#233554',
+        subHeading:'#A8B2D1'
+      },
+    },
+    fontFamily:{
+      'libra':['Libre Franklin', 'sans-serif'],
+    }
+  },
+  plugins: [],
+}
