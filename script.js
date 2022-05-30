13737791b1e56bdde4ee2e60d2eddbabc5da7481
Musfirah Hamid <29musfirah@gmail.com>
2022-05-31 01:02:49 +0500

    musfirah

---
+++ script.js
@@ -0,0 +1,85 @@
+ //navigation Bar
+ function toggleNavbar(collapseID) {
+    document.getElementById(collapseID).classList.toggle("hidden");
+    document.getElementById(collapseID).classList.toggle("block");
+  }
+
+  //Show More toggle
+  $('#show-more-content').hide();
+
+$('#show-more').click(function(){
+  $('#show-more-content').show(300);
+  $('#show-less').show();
+  $('#show-more').hide();
+});
+
+$('#show-less').click(function(){
+  $('#show-more-content').hide(150);
+  $('#show-more').show();
+  $(this).hide();
+});
+
+
+//contact
+
+// Variable to hold request
+var request;
+
+// Bind to the submit event of our form
+$("#register").submit(function(event){
+
+  // Abort any pending request
+  if (request) {
+      request.abort();
+  }
+  // setup some local variables
+  var $form = $(this);
+
+  // Let's select and cache all the fields
+  var $inputs = $form.find("input, select, button, textarea");
+
+  // Serialize the data in the form
+  var serializedData = $form.serialize();
+
+  // Let's disable the inputs for the duration of the Ajax request.
+  // Note: we disable elements AFTER the form data has been serialized.
+  // Disabled form elements will not be serialized.
+  $inputs.prop("disabled", true);
+
+  // Fire off the request to /form.php
+  request = $.ajax({
+      url: "https://script.google.com/macros/s/AKfycbxynqwrnVtFqBKV_qqu8WPCBWsw0X0PSfIF_hEFsoz-_-d5uTf3XLIjDQagtCHfBiQ/exec",
+      type: "post",
+      data: serializedData
+  });
+
+  // Callback handler that will be called on success
+  request.done(function (response, textStatus, jqXHR){
+      // Log a message to the console
+      console.log("Hooray, it worked!");
+      console.log(response);
+      console.log(textStatus);
+      console.log(jqXHR);
+    $("#register")[0].reset();
+  });
+
+  // Callback handler that will be called on failure
+  request.fail(function (jqXHR, textStatus, errorThrown){
+      // Log the error to the console
+      console.error(
+          "The following error occurred: "+
+          textStatus, errorThrown
+      );
+  });
+
+  // Callback handler that will be called regardless
+  // if the request failed or succeeded
+  request.always(function () {
+      // Reenable the inputs
+      $inputs.prop("disabled", false);
+        
+  });
+
+  // Prevent default posting of form
+  event.preventDefault();
+});
